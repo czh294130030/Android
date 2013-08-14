81af707aa8d9d8e913b197361258b8c3bef32d83
U-SUZSOFT\Chad.Cao <Chad.Cao@SZ11100025.suzsoft.com>
2013-08-14 16:44:32 +0800

    second commit

---
+++ androidapp/src/com/example/androidapp/StartupIntentReceiver.java
@@ -0,0 +1,17 @@
+package com.example.androidapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/*捕捉android.intent.action.BOOT_COMPLETED的Receiver类*/
+public class StartupIntentReceiver extends BroadcastReceiver{
+	
+	@Override
+	public void onReceive(Context context, Intent intent){
+		/*当收到Receiver时，指定打开此程序*/
+		Intent mBootIntent=new Intent(context,MainActivity.class);
+		mBootIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(mBootIntent);
+	}
+}
